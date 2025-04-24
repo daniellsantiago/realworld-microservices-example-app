10c2f8e936a0f2dc71539d7ccb7d1d76bda8d27f
Daniel Santiago <daniel.psantiago@mercadolivre.com>
2025-04-24 19:50:02 -0300

    feat: add helth check endpoint

--- users-service/internal/infrastructure/web/routes.go
+++ users-service/internal/infrastructure/web/routes.go
@@ -1,6 +1,13 @@
 package web
 
+import "github.com/gin-gonic/gin"
+
 func SetupRoutes(server *Server) {
+	// Health check endpoint for LB
+	server.Router.GET("/health", func(c *gin.Context) {
+		c.Status(200)
+	})
+
 	routes := server.Router.Group("/api/users")
 	routes.POST("", server.Handlers.CreateUser)
 }
